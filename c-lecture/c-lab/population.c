cf864918e21d099263fa056a44d9916774acbb38
Rin Kates <erinkates11@gmail.com>
2023-09-13 20:10:38 -0400

    lab complete

--- c-lecture/c-lab/population.c
+++ c-lecture/c-lab/population.c
@@ -5,3 +5,42 @@
         // If the user enters a number less than the starting population size, the user should be re-prompted to enter an ending population size until they enter a number that is greater than or equal to the starting population size. (After all, we want the population of llamas to grow!)
     // Your program should then calculate the (integer) number of years required for the population to reach at least the size of the end value.
     // Finally, your program should print the number of years required for the llama population to reach that end size, as by printing to the terminal Years: n, where n is the number of years.
+
+    #include <stdio.h>
+
+int main(void)
+{
+    int start_size;
+    int end_size;
+
+    // Prompt for start size
+    do
+    {
+        printf("Enter starting population size (must be >= 9): ");
+        scanf("%d", &start_size);
+    }
+    while (start_size < 9);
+
+    // Prompt for end size
+    do
+    {
+        printf("Enter ending population size (must be >= starting size): ");
+        scanf("%d", &end_size);
+    }
+    while (end_size < start_size);
+
+    // Calculate number of years until we reach the threshold
+    int years = 0;
+    while (start_size < end_size)
+    {
+        int births = start_size / 3;
+        int deaths = start_size / 4;
+        start_size = start_size + births - deaths;
+        years++;
+    }
+
+    // Print the number of years
+    printf("Years: %d\n", years);
+
+    return 0;
+}
